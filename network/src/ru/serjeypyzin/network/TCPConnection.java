d9dc6e260d52335b2d90f54d66e63c99f1ccab45
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-16 16:00:34 +0300

    changed all files in network module

--- network/src/ru/serjeypyzin/network/TCPConnection.java
+++ network/src/ru/serjeypyzin/network/TCPConnection.java
@@ -4,14 +4,21 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.Charset;
 
-public class TCPConnection  {
+public class TCPConnection {
 
     private final Socket socket;
     private final Thread rxThread;
+    private final TCPConnectionListener eventListener;
     private final BufferedReader in;
     private final BufferedWriter out;
 
-    public TCPConnection (Socket socket) throws IOException {
+    public TCPConnection(TCPConnectionListener eventListener, String ipAddr, int port) throws IOException {
+
+        this(eventListener, new Socket(ipAddr, port));
+    }
+
+    public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
+        this.eventListener = eventListener;
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
@@ -19,16 +26,44 @@ public class TCPConnection  {
         rxThread = new Thread(new Runnable() {
             @Override
             public void run() {
-               try {
-                   String msg = in.readLine();
-               } catch (IOException e) {
-
-               } finally {
-
-               }
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while (!rxThread.isInterrupted()) {
+                        eventListener.onReceiveString(TCPConnection.this, in.readLine());
+                    }
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                } finally {
+                    eventListener.onDisconnect(TCPConnection.this);
+                }
             }
         });
         rxThread.start();
     }
 
+    public synchronized void sendString(String value) {
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect();
+        }
+    }
+
+    public synchronized void disconnect() {
+
+        rxThread.isInterrupted();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
+    }
 }
--- network/src/ru/serjeypyzin/network/TCPConnectionListener.java
+++ network/src/ru/serjeypyzin/network/TCPConnectionListener.java
@@ -4,7 +4,7 @@ public interface TCPConnectionListener {
 
     void onConnectionReady(TCPConnection tcpConnection);
     void onReceiveString(TCPConnection tcpConnection, String value);
-    void onDisconnect(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
     void onException(TCPConnection tcpConnection, Exception e);
 
 
