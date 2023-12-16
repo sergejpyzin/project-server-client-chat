228de03a08e2e7497195a5adc5c9ac6473fdff0e
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-16 12:07:18 +0300

    created TCPConnection.java

--- network/src/ru/serjeypyzin/network/TCPConnection.java
+++ network/src/ru/serjeypyzin/network/TCPConnection.java
@@ -1,4 +1,34 @@
 package ru.serjeypyzin.network;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
 public class TCPConnection  {
+
+    private final Socket socket;
+    private final Thread rxThread;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+
+    public TCPConnection (Socket socket) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               try {
+                   String msg = in.readLine();
+               } catch (IOException e) {
+
+               } finally {
+
+               }
+            }
+        });
+        rxThread.start();
+    }
+
 }
