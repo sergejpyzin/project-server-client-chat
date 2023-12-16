3643df153d2108b0dacf1de914e9db1d3d4163f6
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-16 19:18:20 +0300

    finished work

--- network/src/ru/serjeypyzin/network/TCPConnection.java
+++ network/src/ru/serjeypyzin/network/TCPConnection.java
@@ -2,7 +2,7 @@ package ru.serjeypyzin.network;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class TCPConnection {
 
@@ -20,8 +20,8 @@ public class TCPConnection {
     public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
         this.eventListener = eventListener;
         this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
 
         rxThread = new Thread(new Runnable() {
             @Override
@@ -52,14 +52,12 @@ public class TCPConnection {
     }
 
     public synchronized void disconnect() {
-
         rxThread.isInterrupted();
         try {
             socket.close();
         } catch (IOException e) {
             eventListener.onException(TCPConnection.this, e);
         }
-
     }
 
     @Override
